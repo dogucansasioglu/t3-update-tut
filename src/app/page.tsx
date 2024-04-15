ce27d6159c18109ca3855e86f847538a6753be16
Doğucan Şaşıoğlu <sasiogludogucan@gmail.com>
2024-04-15 11:29:00 +0300

    link database to ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,36 +2,25 @@ import { db } from "@/server/db";
 
 export const dynamic = "force-dynamic";
 
-const imagesUrl = [
-  "https://utfs.io/f/bb57cad2-c785-4338-91e5-6e7b08d4848d-f51xlm.webp",
-  "https://utfs.io/f/61e425df-0f3a-4ad4-825b-e74862cbcefb-1wbh7b.webp",
-  "https://utfs.io/f/8e5904f1-0fb1-4f69-88af-c6d5b1e54989-5plx5.webp",
-  "https://utfs.io/f/4a241588-44a2-4785-b2f4-b308c0d9a888-av9pqn.webp",
-];
-
-const images = imagesUrl.map((url, index) => ({
-  id: index + 1,
-  src: url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id} className="h-48 w-48">
-            {post.name}
-          </div>
-        ))}
         {[...images, ...images, ...images].map((image, index) => (
-          <img
-            key={index}
-            src={image.src}
-            alt={`Image ${index + 1}`}
-            className="h-48 w-48"
-          />
+          <div className="flex flex-col" key={image.id + "-" + index}>
+            <img
+              src={image.url}
+              alt={`Image ${index + 1}`}
+              className="h-48 w-48"
+            />
+            <div>
+              <p className="text-sm">{image.name}</p>
+            </div>
+          </div>
         ))}
       </div>
     </main>
